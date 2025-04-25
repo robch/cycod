500c55cec701cfb31d77494e0bd5689658dad389
Rob Chambers <robc@thinkman.com>
2025-04-24 18:07:12 -0700

    more helpers

--- src/common/Helpers/EnvVarSettingException.cs
+++ src/common/Helpers/EnvVarSettingException.cs
@@ -7,7 +7,7 @@ using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 
-internal class EnvVarSettingException : Exception
+public class EnvVarSettingException : Exception
 {
     public EnvVarSettingException() : base()
     {
