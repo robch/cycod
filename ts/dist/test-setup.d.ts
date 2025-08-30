ff503929ee81639fecdcac4babae0e270d4b2324
Philipp Schmid <philipp.h.schmid@gmail.com>
2025-08-29 22:08:34 -0700

    removed files in ts/dist

--- ts/dist/test-setup.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const createTempDir: () => Promise<string>;
-export declare const cleanupTempDir: (tempDir: string) => Promise<void>;
-export declare const mockConsole: () => {
-    logs: string[];
-    errors: string[];
-    restore: () => void;
-};
-//# sourceMappingURL=test-setup.d.ts.map
